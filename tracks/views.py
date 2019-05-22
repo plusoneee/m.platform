8abbe7240aea83d6311453397cb26b96f1e75dc6
plusoneee <y02121148s@gmail.com>
2019-05-22 14:40:30 +0800

    check if user liked

--- tracks/views.py
+++ tracks/views.py
@@ -27,23 +27,37 @@ def music_record_logs(request):
 @csrf_exempt
 def add_to_playlist(request):
     if request.method == 'POST':
+        
         data = json.loads(request.body)
         user = data['user']
         track_info = data['track'].split(':')
-        ablum_id = track_info[0]
-        track_id = track_info[1]
-        print(user, track_id, ablum_id)
-        return HttpResponse(request.body)
+        print(track_info)
+        ablum_id, track_id = track_info[0], track_info[1]
+        row = (user, track_id, ablum_id)
+        sql = "INSERT INTO tracks.tracks_userslike (`user`, `track`, `album`) VALUES "+ str(row) + ";"
+        run_sql_cmd(sql)
+        with connection.cursor() as cursor:
+                cursor.execute(sql)
+        return HttpResponse('Add')
+
+@csrf_exempt
+def remove_from_playlist(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        user = data['user']
+        track_info = data['track'].split(':')
+        ablum_id, track_id = track_info[0], track_info[1]
+        sql = "delete from tracks_userslike where user ='" + user+ "' and track ='"+ track_id +"' and album ='" +ablum_id+ "';"
+        with connection.cursor() as cursor:
+                cursor.execute(sql)
+        return HttpResponse('Remove')
 
 
 @login_required
 def browser_artist(request):
     # Every time user refresh page will get 30 different artists.
     sql = 'select * from tracks_artist order by rand() limit 30'
-    with connection.cursor() as cursor:
-        cursor.execute(sql)
-        dict_row = dictfetchall(cursor)
-
+    dict_row = run_sql_cmd(sql)
     return render(request,'browserArtist.html',{
         'artist_list':dict_row,
     })
@@ -51,10 +65,7 @@ def browser_artist(request):
 @login_required
 def browser_artist_from_letter(request, letter):
     sql = "SELECT * from  tracks.tracks_artist where Name LIKE '" + letter +"%'"
-    print(sql)
-    with connection.cursor() as cursor:
-        cursor.execute(sql)
-        dict_row = dictfetchall(cursor)
+    dict_row = run_sql_cmd(sql)
     return render(request,'selected_artist.html',{
         'artist_list':dict_row,
     })
@@ -62,46 +73,57 @@ def browser_artist_from_letter(request, letter):
 @login_required
 def artist_info(request, artist_id):
     sql = "select * from tracks_album where artist_id ='" + artist_id +"'ORDER BY release_date DESC;"
-    with connection.cursor() as cursor:
-        cursor.execute(sql)
-        dict_row = dictfetchall(cursor)
+    dict_row = run_sql_cmd(sql)
     return render(request,'artistInfo.html',{
         'artist_album':dict_row,
     })
 
 @login_required
 def ablum_info(request, album_id):
-#     print(album_id)
-    with connection.cursor() as cursor:
-        sql = "select  M.artist, M.artist_id, M.album_name, M.album_id,  M.img, features.id, features.name, features.preview_url from tracks_features as features INNER JOIN  ( \
-                SELECT artist.name as artist, artist.id as artist_id, album.name as album_name, \
-                album.img_url as img, album.id as album_id \
-                FROM tracks_artist as artist \
-                INNER JOIN tracks_album as album \
-                where album.artist_id = artist.id) M \
-                where M.album_id = features.album_id and M.album_id = '"+album_id +"';"
-        cursor.execute(sql)
-        dict_row = dictfetchall(cursor)
+#     user_name = request.user
+    user_name = request.user    
+    sql = "select track_id, artist, artist_id, album_name, album_id, img, name, preview_url, user  from ( \
+	select  features.id as track_id, M.artist, M.artist_id, M.album_name, M.album_id,  M.img, features.id, features.name, features.preview_url \
+	from tracks_features as features INNER JOIN  ( \
+	SELECT artist.name as artist, artist.id as artist_id, album.name as album_name, \
+	album.img_url as img, album.id as album_id \
+	FROM tracks_artist as artist \
+	INNER JOIN tracks_album as album \
+	where album.artist_id = artist.id) M \
+        where M.album_id = features.album_id and M.album_id = '" + album_id + "' ) Album \
+	left Join tracks.tracks_userslike as L  on L.track = Album.id and L.user = '" + str(user_name) + "'"
+    dict_row = run_sql_cmd(sql)
+    
+    for item in dict_row:
+        if item['user'] == None:
+                item['user'] = ''
     return render(request,'album.html',{
         'album':dict_row,
     })
 
 
-def sql_join_table_example(request):
-    with connection.cursor() as cursor:
-        sql = "select * from tracks_features as features INNER JOIN  ( \
-                SELECT artist.name as artist, artist.id as artist_id, album.name as album_name, \
-                album.img_url as img, album.id as album_id \
-                FROM tracks_artist as artist \
-                INNER JOIN tracks_album as album where album.artist_id = artist.id) M where M.album_id = features.album_id;" 
-        cursor.execute(sql)
-        dict_row = dictfetchall(cursor)
-    return HttpResponse(
-                json.dumps(dict_row, sort_keys=False, indent=2), 
-                content_type="application/json")  
-
 def dictfetchall(cursor):
     "Return all rows from a cursor as a dict"
     columns = [col[0] for col in cursor.description]
+    
     return [dict(zip(columns, row))
         for row in cursor.fetchall()]
+
+def run_sql_cmd(sql):
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+        return dictfetchall(cursor)
+
+'''
+def sql_join_table_example(request):
+    sql = "select * from tracks_features as features INNER JOIN  ( \
+            SELECT artist.name as artist, artist.id as artist_id, album.name as album_name, \
+            album.img_url as img, album.id as album_id \
+            FROM tracks_artist as artist \
+            INNER JOIN tracks_album as album where album.artist_id = artist.id) M where M.album_id = features.album_id;" 
+    dict_row = run_sql_cmd(sql)
+    
+    return HttpResponse(
+                json.dumps(dict_row, sort_keys=False, indent=2), 
+                content_type="application/json")  
+'''
